97991a5598f0063285675bd7ec2477701691fd31
koukibuu3 <koukibuu3@gmail.com>
2024-04-19 01:00:43 +0900

    feat: マンデルブロ集合: 無限ループを作成

---
+++ mandelbrot/src/main.rs
@@ -0,0 +1,18 @@
+use num::Complex;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn square_loop(mut x: f64) {
+    loop {
+        x = x * x;
+    }
+}
+
+fn square_add_loop(c: f64) {
+    let mut x = 0.;
+    loop {
+        x = x * x + c;
+    }
+}
