93d9df9f11b9cd44bbd6090c8d4b03fbad4af360
koukibuu3 <koukibuu3@gmail.com>
2024-04-20 09:40:40 +0900

    feat: 指定した文字列を置換する処理

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -32,6 +32,13 @@ fn parse_args() -> Arguments {
     }
 }
 
+use regex::Regex;
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
+}
+
 fn main() {
     let args = parse_args();
 
@@ -43,7 +50,15 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, &data) {
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        },
+    };
+
+    match fs::write(&args.output, &replaced_data) {
         Ok(_) => {},
         Err(e) => {
             eprintln!("{} failed to write to file '{}': {:?}", "Error:".red().bold(), args.filename, e);
